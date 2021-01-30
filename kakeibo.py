42c23ad838bf2c909f7531213b86133a28cd2128
iijimal825 <77562094+iijimal825@users.noreply.github.com>
2021-01-31 00:36:13 +0900

    Develop #1 (#4)
    * :art: make class for data analyzer
    
    * :art: add pd.read_csv
    
    * :art: fix #1

--- kakeibo.py
+++ kakeibo.py
@@ -1,274 +1,154 @@
 #!/usr/bin/python3
-#!python3.7
 # -*- coding: utf-8 -*-
-import os,sys
-from tkinter import *
+
+import os
+import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
-import csv
 import pandas as pd
 
-def categorize_for_person(row):
-    if row[6] == '共通':
-        common_carfare += int(row[3])
-    elif row[6] == 'ともこ':
-        tomoko_carfare += int(row[3])
-    elif row[6] == 'るな':
-        luna_carfare += int(row[3])
-    else:
-        other_carfare += int(row[3])
 
-def analyze_data(incsv):
-    common_food = 0
-    tomoko_food = 0
-    luna_food = 0
-    eat_out = 0
-    common_livingware = 0
-    tomoko_livingware = 0
-    luna_livingware = 0
-    other_livingware = 0
-    common_housing = 0
-    tomoko_housing = 0
-    luna_housing = 0
-    other_housing = 0
-    common_carfare = 0
-    tomoko_carfare = 0
-    luna_carfare = 0
-    other_carfare = 0
-    common_phone = 0
-    tomoko_phone = 0
-    luna_phone = 0
-    other_phone = 0
-    common_energy = 0
-    tomoko_energy = 0
-    luna_energy = 0
-    other_energy = 0
-    common_edu = 0
-    tomoko_edu = 0
-    luna_edu = 0
-    other_edu = 0
-    common_hobby = 0
-    tomoko_hobby = 0
-    luna_hobby = 0
-    yukimaru_hobby = 0
-    ham_hobby = 0
-    other_hobby = 0
-    common_clothing = 0
-    tomoko_clothing = 0
-    luna_clothing = 0
-    other_clothing = 0
-    common_med = 0
-    tomoko_med = 0
-    luna_med = 0
-    other_med = 0
-    common_gift = 0
-    tomoko_gift = 0
-    luna_gift = 0
-    other_gift = 0
-    common_social = 0
-    tomoko_social = 0
-    luna_social = 0
-    other_social = 0
-    other_cost = 0
-    all_cost = 0
-    with open(incsv) as rf:
-        reader = csv.reader(rf)
-        for row in reader:
-            if row[1] == '支出':
-                all_cost += int(row[2])
-                if row[3] == '食費':
-                    if row[7] == '外食':
-                        eat_out += int(row[2])
-#                    else:
-#                        eat_out += int(row[2])
-                    elif row[6] == '共通':
-                        common_food += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_food += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_food += int(row[2])
-                    else:
-                        eat_out += int(row[2])
+class Analyze:
+    def __init__(self):
+        self.cat_list = [
+            '食費', '生活雑費', '住居費', '交通費', '通信費', '光熱費',
+            '教育教養費', '娯楽費', '被服費', '医療費',
+            'ギフト・プレゼント', '交際費', 'その他'
+        ]
+        self.user_list = ['共通', 'ともこ', 'るな', 'ゆきまる', 'ハム', 'その他']
+        self.ratio_tmk_list = [
+            0.6, 1, 0, 0.5, 1, 1
+        ]
+        self.payer_dict = {
+            'ともこ': [0.6, 1, 0, 0.5, 1, 1],
+            'るな': [0.4, 0, 1, 0.5, 0, 0]
+        }
 
-                elif row[3] == '生活雑費':
-                    if row[6] == '共通':
-                        common_livingware += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_livingware += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_livingware += int(row[2])
-                    else:
-                        other_livingware += int(row[2])
-                elif row[3] == '住居費':
-                    if row[6] == '共通':
-                        common_housing += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_housing += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_housing += int(row[2])
-                    else:
-                        other_housing += int(row[2])
-                elif row[3] == '交通費':
-                    if row[6] == '共通':
-                        common_carfare += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_carfare += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_carfare += int(row[2])
-                    else:
-                        other_carfare += int(row[2])
-                elif row[3] == '通信費':
-                    if row[6] == '共通':
-                        common_phone += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_phone += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_phone += int(row[2])
-                    else:
-                        other_phone += int(row[2])
-                elif row[3] == '光熱費':
-                    if row[6] == '共通':
-                        common_energy += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_energy += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_energy += int(row[2])
-                    else:
-                        other_energy += int(row[2])
-                elif row[3] == '教育教養費':
-                    if row[6] == '共通':
-                        common_edu += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_edu += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_edu += int(row[2])
-                    else:
-                        other_edu += int(row[2])
-                elif row[3] == '娯楽費':
-                    if row[6] == '共通':
-                        common_hobby += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_hobby += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_hobby += int(row[2])
-                    elif row[6] == 'ゆきまる':
-                        yukimaru_hobby += int(row[2])
-                    elif row[6] == 'ハム':
-                        ham_hobby += int(row[2])
-                    else:
-                        other_hobby += int(row[2])
-                elif row[3] == '被服費':
-                    if row[6] == '共通':
-                        common_clothing += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_clothing += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_clothing += int(row[2])
-                    else:
-                        other_clothing += int(row[2])
-                elif row[3] == '医療費':
-                    if row[6] == '共通':
-                        common_med += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_med += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_med += int(row[2])
-                    else:
-                        other_med += int(row[2])
-                elif row[3] == 'ギフト・プレゼント':
-                    if row[6] == '共通':
-                        common_gift += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_gift += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_gift += int(row[2])
-                    else:
-                        other_gift += int(row[2])
-                elif row[3] == '交際費':
-                    if row[6] == '共通':
-                        common_social += int(row[2])
-                    elif row[6] == 'ともこ':
-                        tomoko_social += int(row[2])
-                    elif row[6] == 'るな':
-                        luna_social += int(row[2])
-                    else:
-                        other_social += int(row[2])
-                else:
-                    other_cost += int(row[2])
+    def read_csv(self, incsv):
+        # yen-note csv を DataFrame に変換する
+        df_csv = pd.read_csv(incsv, encoding='shift-jis')
+        # 支出のみ抽出する
+        df_expence = df_csv.query('金額 < 0')
+        return df_expence
+
+    def sum_normal(self, df):
+        sum_list = []
+        # user ごとの合計を算出する
+        for user in self.user_list:
+            df_user = df[df['利用者'].str.contains(user)]
+            cost = df_user['金額'].sum()
+            sum_list.append(cost)
+        # 除外された行の合計をその他に合算する
+        df_other = df[~df['利用者'].isin(self.user_list)]
+        sum_list[-1] += df_other['金額'].sum()
+        return sum_list
+
+    def exclude_eat_out(self, df):
+        # 外食はその他に加算する
+        cost = df[df['ショップ'].str.contains('外食')]['金額'].sum()
+        df_after = df[~df['ショップ'].isin(['外食'])]
+        return cost, df_after
+
+    def edit_sum_row(self, row_len, col_len):
+        # アルファベット大文字の list を取得
+        abc_list = [chr(i) for i in range(65, 65+26)]
+        # SUM 関数を作成
+        sum_list = []
+        for i in range(col_len):
+            col = abc_list[i+1]
+            str_sum = '=SUM({0}{1}:{0}{2})'.format(col, 2, row_len+1)
+            sum_list.append(str_sum)
+        return sum_list
+
+    def edit_ratio_row(self, row_len, col_len, ratio_list):
+        # アルファベット大文字の list を取得
+        abc_list = [chr(i) for i in range(65, 65+26)]
+        # 支払い割合行を作成
+        sum_list = []
+        for i, ratio in enumerate(ratio_list):
+            col = abc_list[i+1]
+            str_sum = '={0}{1}*{2}'.format(col, row_len+1, ratio)
+            sum_list.append(str_sum)
+        return sum_list
+
+    def edit_sum_col(self, row_len, col_len):
+        # アルファベット大文字の list を取得
+        abc_list = [chr(i) for i in range(65, 65+26)]
+        col1 = abc_list[1]
+        col2 = abc_list[col_len]
+        # SUM 関数を作成
+        sum_list = []
+        for i in range(row_len):
+            str_sum = '=SUM({0}{2}:{1}{2})'.format(col1, col2, i+2)
+            sum_list.append(str_sum)
+        return sum_list
+
+    def run(self, incsv):
+        df = self.read_csv(incsv)
+        # カテゴリごとに集計
+        self.sum_dict = {}
+        self.output_list = []
+        for category in self.cat_list:
+            # 初期化する
+            sum_list = []
+            # 該当 category DataFrame を作成する
+            df_cat = df[df['カテゴリ'].str.contains(category)]
+            if category == '食費':
+                cost_eat_out, df_cat = self.exclude_eat_out(df_cat)
+                sum_list = self.sum_normal(df_cat)
+                sum_list[-1] += cost_eat_out
+            elif category == 'その他':
+                for i in range(len(self.user_list)):
+                    sum_list.append(0)
+                sum_list[-1] += df_cat['金額'].sum()
             else:
-                pass
-    print(all_cost)
-    common_list = [common_food, common_livingware, common_housing,\
-                   common_carfare, common_phone, common_energy,\
-                   common_edu, common_hobby, common_clothing, common_med,\
-                   common_gift, common_social, 0, "=SUM(B2:N2)",\
-                   ".", "=O2*0.4", "=O2*0.6"]
-    tomoko_list = [tomoko_food, tomoko_livingware, tomoko_housing,\
-                   tomoko_carfare, tomoko_phone, tomoko_energy,\
-                   tomoko_edu, tomoko_hobby, tomoko_clothing, tomoko_med,\
-                   tomoko_gift, tomoko_social, 0, "=SUM(B3:N3)",\
-                   ".", ".", "=O3"]
-    luna_list = [luna_food, luna_livingware, luna_housing,\
-                 luna_carfare, luna_phone, luna_energy,\
-                 luna_edu, luna_hobby, luna_clothing, luna_med,\
-                 luna_gift, luna_social, 0, "=SUM(B4:N4)",\
-                 ".", "=O4", "."]
-    yukimaru_list = [0, 0, 0, 0, 0, 0, 0, yukimaru_hobby,\
-                     0, 0, 0, 0, 0, "=SUM(B5:N5)",\
-                     ".", "=O5*0.5", "=O5*0.5"]
-    ham_list = [0, 0, 0, 0, 0, 0, 0, ham_hobby,\
-                0, 0, 0, 0, 0, "=SUM(B6:N6)",\
-                ".", ".", "=O6"]
-    other_list = [eat_out, other_livingware, other_housing,\
-                  other_carfare, other_phone, other_energy,\
-                  other_edu, other_hobby, other_clothing, other_med,\
-                  other_gift, other_social, other_cost, "=SUM(B7:N7)",\
-                  ".", ".", "=O7"]
-    sum_list = ['=SUM(B2:B7)', '=SUM(C2:C7)', '=SUM(D2:D7)', '=SUM(E2:E7)', \
-                '=SUM(F2:F7)', '=SUM(G2:G7)', '=SUM(H2:H7)', '=SUM(I2:I7)', \
-                '=SUM(J2:J7)', '=SUM(K2:K7)', '=SUM(L2:L7)', '=SUM(M2:M7)', \
-                '=SUM(N2:N7)', '=SUM(O2:O7)', '.', '=SUM(Q2:Q7)', '=SUM(R2:R7)']
-    data_frame_list = [common_list, tomoko_list, luna_list, \
-                       yukimaru_list, ham_list, other_list, sum_list]
-
-    col_list = ['食費', '生活雑貨', '住居費', '交通費', '通信費', '光熱費',\
-                '教育教養費', '娯楽費', '被服費', '医療費', 'ギフト', '交際費', 'その他', '合計',\
-                '.', 'るな', 'ともこ']
-    index_list = ['共通', 'ともこ', 'るな', 'ゆきまる', 'ハム', 'その他', '合計']
-    # 出力ファイル名の取得
-    outxlsx = os.path.splitext(incsv)[0] + '_done.xlsx'
-    df = pd.DataFrame(data_frame_list, index=index_list, columns=col_list)
-#    df.to_excel(outxlsx, sheet_name='家計簿')
-    with pd.ExcelWriter(outxlsx) as wf:
-        df.to_excel(wf, sheet_name='sheet1')
-
-    return outxlsx
- #   with open(outcspy
- # v, 'w') as wf:
- #       writer = csv.writer(f)
- #       writer.writerow(['#', '食費', '生活雑貨', '住居費', '交通費', '通信費', '光熱費',\
- #                        '教育教養費', '娯楽費', '被服費', '医療費', 'ギフト', '交際費', 'その他'])
- #       writer.writerow(['a', 'b', 'c'])
+                sum_list = self.sum_normal(df_cat)
+            self.sum_dict[category] = sum_list
+            self.output_list.append(sum_list)
+        # 合計行を作成する
+        sum_list = self.edit_sum_row(len(self.cat_list), len(self.user_list))
+        self.output_list.append(sum_list)
+        self.cat_list.append('合計')
+        # 支払者行を作成する
+        sum_row_num = len(self.cat_list)
+        for i, (payer, ratio_list) in enumerate(self.payer_dict.items()):
+            sum_list = self.edit_ratio_row(sum_row_num, len(self.user_list), ratio_list)
+            self.output_list.append(sum_list)
+            self.cat_list.append(payer)
+        # 合計列を作成する
+        row_sum_list = self.edit_sum_col(len(self.cat_list), len(self.user_list))
+        for i, cat_sum_list in enumerate(self.output_list):
+            cat_sum_list.append(row_sum_list[i])
+        self.user_list.append('合計')
+        # 出力ファイル名の取得
+        outxlsx = os.path.splitext(incsv)[0] + '_done.xlsx'
+        df = pd.DataFrame(self.output_list, index=self.cat_list, columns=self.user_list)
+        with pd.ExcelWriter(outxlsx) as wf:
+            df.to_excel(wf, sheet_name='sheet1')
+        return outxlsx
 
 
 # 参照ボタンのイベント
 # button1クリック時の処理
 def button1_clicked():
-    fTyp = [("","*")]
+    fTyp = [("", "*")]
     iDir = os.path.abspath(os.path.dirname(__file__))
-    filepath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+    filepath = filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
     file1.set(filepath)
+
 
 # button2クリック時の処理
 def button2_clicked():
     incsv = file1.get()
-    outxlsx = analyze_data(incsv)
+    analyze = Analyze()
+    outxlsx = analyze.run(incsv)
     messagebox.showinfo('正常終了', u'出力ファイルは\n' + outxlsx)
+
 
 if __name__ == '__main__':
     # rootの作成
-    root = Tk()
+    root = tk.Tk()
     root.title('FileReference Tool')
     root.resizable(False, False)
 
@@ -282,26 +162,26 @@ if __name__ == '__main__':
 
     # ラベルの作成
     # 「ファイル」ラベルの作成
-    s = StringVar()
+    s = tk.StringVar()
     s.set('ファイル>>')
     label1 = ttk.Label(frame1, textvariable=s)
     label1.grid(row=0, column=0)
 
     # 参照ファイルパス表示ラベルの作成
-    file1 = StringVar()
+    file1 = tk.StringVar()
     file1_entry = ttk.Entry(frame1, textvariable=file1, width=50)
     file1_entry.grid(row=0, column=2)
 
     # Frame2の作成
-    frame2 = ttk.Frame(root, padding=(0,5))
+    frame2 = ttk.Frame(root, padding=(0, 5))
     frame2.grid(row=1)
 
     # Startボタンの作成
     button2 = ttk.Button(frame2, text='Start', command=button2_clicked)
-    button2.pack(side=LEFT)
+    button2.pack(side=tk.LEFT)
 
     # Cancelボタンの作成
     button3 = ttk.Button(frame2, text='Cancel', command=quit)
-    button3.pack(side=LEFT)
+    button3.pack(side=tk.LEFT)
 
     root.mainloop()
